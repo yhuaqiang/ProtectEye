a04a8da742773a1f96687c3a6365020feee3b7fc
yhuaqiang <yhuaqiang@163.com>
2018-05-11 23:06:28 +0800

    重新开发

--- ProtectEye/Program.cs
+++ ProtectEye/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProtectEye.form;
+using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
 
---
+++ ProtectEye/form/MainForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProtectEye.form
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
